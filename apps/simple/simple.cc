5906520466c9b1cf6e363c8f2871653a03b24f57
saubhik <saubhik@node-0.experiment-0.quic-server-pg0.utah.cloudlab.us>
2021-09-06 10:57:04 -0600

    add Makefile

--- apps/simple/simple.cc
+++ apps/simple/simple.cc
@@ -1,5 +1,6 @@
 extern "C" {
 #include <base/log.h>
+#include <net/ip.h>
 }
 
 #include "runtime.h"
@@ -8,12 +9,15 @@ extern "C" {
 #include <string>
 #include <memory>
 
+netaddr raddr;
+constexpr uint64_t simplePort = 8001;
+
 void ServerHandler(void *arg) {
-    std::unique_ptr<rt::TcpQueue> q(rt::TcpQueue::Listen(laddr, 4096));
+    std::unique_ptr<rt::TcpQueue> q(rt::TcpQueue::Listen({0, simplePort}, 4096));
     if (q == nullptr) panic("couldn't listen for connections");
 
     while (true) {
-        rt:TcpConn *c = q->Accept();
+        rt::TcpConn *c = q->Accept();
         if (c == nullptr) panic("couldn't accept a connection");
     }
 }
@@ -21,6 +25,15 @@ void ServerHandler(void *arg) {
 void ClientHandler(void *arg) {
     std::unique_ptr<rt::TcpConn> conn(rt::TcpConn::Dial({0, 0}, raddr));
     if (unlikely(conn == nullptr)) panic("couldn't connect to raddr");
+}
+
+int StringToAddr(const char *str, uint32_t *addr) {
+  uint8_t a, b, c, d;
+
+  if (sscanf(str, "%hhu.%hhu.%hhu.%hhu", &a, &b, &c, &d) != 4) return -EINVAL;
+
+  *addr = MAKE_IP_ADDR(a, b, c, d);
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -34,6 +47,10 @@ int main(int argc, char *argv[]) {
             return ret;
         }
     }
+
+    ret = StringToAddr(argv[3], &raddr.ip);
+    if (ret) return -EINVAL;
+    raddr.port = simplePort;
 
     ret = runtime_init(argv[1], ClientHandler, NULL);
     if (ret) {
