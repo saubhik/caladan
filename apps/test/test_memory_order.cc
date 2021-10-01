48b5839d04ac07334ea5f1c3b67b8aa1ccb7d33a
Saubhik Mukherjee <saubhik.mukherjee@gmail.com>
2021-09-30 19:52:47 -0600

    complete implementation of thread id

--- apps/test/test_memory_order.cc
+++ apps/test/test_memory_order.cc
@@ -13,6 +13,7 @@ extern "C" {
 
 #include <atomic>
 #include <iostream>
+#include <sstream>
 
 namespace {
 
@@ -20,29 +21,44 @@ std::atomic<bool> x = ATOMIC_VAR_INIT(false);
 std::atomic<bool> y = ATOMIC_VAR_INIT(false);
 std::atomic<int> z = ATOMIC_VAR_INIT(0);
 
-void write_x() { x.store(true, std::memory_order_seq_cst); }
+// Check rt::GetId() works.
+void PrintId() {
+  std::stringstream msg;
+  msg << "My ID is " << rt::GetId() << "\n";
+  std::cout << msg.str();
+}
 
-void write_y() { y.store(true, std::memory_order_seq_cst); }
+void WriteX() {
+  x.store(true, std::memory_order_seq_cst);
+  PrintId();
+}
+
+void WriteY() {
+  y.store(true, std::memory_order_seq_cst);
+  PrintId();
+}
 
 // z == 0 only if x=true, y=false
-void read_x_then_y() {
+void ReadXThenY() {
   while (!x.load(std::memory_order_seq_cst))
     ;
   if (y.load(std::memory_order_seq_cst)) ++z;
+  PrintId();
 }
 
 // z == 0 only if x=false, y=true
-void read_y_then_x() {
+void ReadYThenX() {
   while (!y.load(std::memory_order_seq_cst))
     ;
   if (x.load(std::memory_order_seq_cst)) ++z;
+  PrintId();
 }
 
 void MainHandler(void *arg) {
-  auto th1 = rt::Thread(write_x);
-  auto th2 = rt::Thread(write_y);
-  auto th3 = rt::Thread(read_x_then_y);
-  auto th4 = rt::Thread(read_y_then_x);
+  auto th1 = rt::Thread(WriteX);
+  auto th2 = rt::Thread(WriteY);
+  auto th3 = rt::Thread(ReadXThenY);
+  auto th4 = rt::Thread(ReadYThenX);
 
   th1.Join();
   th2.Join();
