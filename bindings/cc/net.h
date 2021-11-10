ce67fd331a9e5bc8324e4082045ad3e2524bca7e
Saubhik Mukherjee <saubhik.mukherjee@gmail.com>
2021-11-10 13:58:34 -0500

    WIP Make rt::UdpConn compatible with folly netops::

--- bindings/cc/net.h
+++ bindings/cc/net.h
@@ -9,11 +9,32 @@ extern "C" {
 #include <runtime/poll.h>
 }
 
+#include <string>
+
 namespace rt {
+
+netaddr StringToNetaddr(const std::string &str) {
+  // For IPv4 addresses, the string is of the form "<IP>:<port>"
+  netaddr addr{};
+  str_to_netaddr(str.c_str(), &addr);
+  return addr;
+}
+
+std::string NetaddrToIPString(netaddr naddr) {
+  char sip[IP_ADDR_STR_LEN];
+  uint32_t addr = naddr.ip;
+  snprintf(sip, IP_ADDR_STR_LEN, "%d.%d.%d.%d",
+           ((addr >> 24) & 0xff),
+           ((addr >> 16) & 0xff),
+           ((addr >> 8) & 0xff),
+           (addr & 0xff));
+  std::string str(sip);
+  return str;
+}
 
 class NetConn {
  public:
-  virtual ~NetConn(){};
+  virtual ~NetConn() = default;
   virtual ssize_t Read(void *buf, size_t len) = 0;
   virtual ssize_t Write(const void *buf, size_t len) = 0;
 };
@@ -21,8 +42,15 @@ class NetConn {
 // UDP Connections.
 class UdpConn : public NetConn {
   friend class EventLoop;
+
  public:
-  ~UdpConn() { udp_close(c_); }
+  UdpConn() = default;
+  explicit UdpConn(udpconn_t *c) : c_(c) { localAddr_ = nullptr; }
+  ~UdpConn() override { udp_close(c_); }
+
+  // disable move and copy.
+  UdpConn(const UdpConn &) = delete;
+  UdpConn &operator=(const UdpConn &) = delete;
 
   // The maximum possible payload size (with the maximum MTU).
   static constexpr size_t kMaxPayloadSize = UDP_MAX_PAYLOAD_SIZE;
@@ -33,6 +61,13 @@ class UdpConn : public NetConn {
     int ret = udp_dial(laddr, raddr, &c);
     if (ret) return nullptr;
     return new UdpConn(c);
+  }
+
+  int Connect(const netaddr *remoteAddr) {
+    if (localAddr_ == nullptr) {
+      return udp_dial({0, 0}, *remoteAddr, &c_);
+    }
+    return udp_dial(*localAddr_, *remoteAddr, &c_);
   }
 
   // Creates a UDP connection that receives all packets on a local port.
@@ -48,6 +83,9 @@ class UdpConn : public NetConn {
 
   // Gets head of event list
   struct list_head *EventList() { return udp_get_triggers(c_); }
+
+  // Set the bind address
+  void Bind(const netaddr *addr) { localAddr_ = addr; }
 
   // Gets the local UDP address.
   netaddr LocalAddr() const { return udp_local_addr(c_); }
@@ -79,18 +117,13 @@ class UdpConn : public NetConn {
   void Shutdown() { udp_shutdown(c_); }
 
   // Set the socket's nonblocking state
-  void SetNonblocking (bool nonblocking) {
+  void SetNonblocking(bool nonblocking) {
     udp_set_nonblocking(c_, nonblocking);
   }
 
  private:
-  UdpConn(udpconn_t *c) : c_(c) {}
-
-  // disable move and copy.
-  UdpConn(const UdpConn &) = delete;
-  UdpConn &operator=(const UdpConn &) = delete;
-
-  udpconn_t *c_;
+  udpconn_t *c_{};
+  const netaddr *localAddr_;
 };
 
 // TCP connections.
